407c867e21504fa99d7f6e95a24307fe298b4e14
zurg3 <20664125+zurg3@users.noreply.github.com>
2025-09-23 18:35:10 +0300

    Added ability to build CLI version into single-file executable

--- web/cli.js
+++ web/cli.js
@@ -9,8 +9,9 @@ const conf = require('./lib/conf');
 
 const args = process.argv.slice(2);
 
-const resovle = (file) => path.join(__dirname, file);
-const write = (file, content) => fs.writeFile(resovle(file), content, 'utf-8');
+const base_dir = process.pkg ? path.dirname(process.execPath) : __dirname;
+const resovle = (file) => path.join(base_dir, file);
+const write = (file, content) => fs.writeFile(resovle(file), content, 'utf8');
 
 async function get_config() {
   const mode = args[0];
